dfb87efa7e1ea6010a9aba8fc8c4950cfbb9e703
MJ Pooladkhay <mj@pooladkhay.com>
2025-01-23 16:26:48 +0000

    attach qemu monitor to stdio
    Signed-off-by: MJ Pooladkhay <mj@pooladkhay.com>

--- run.sh
+++ run.sh
@@ -9,4 +9,5 @@ CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32 -ffreestanding -nostdlib
 $CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf kernel.c common.c
 
 # Start QEMU
-$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot -kernel kernel.elf
+$QEMU -monitor stdio -machine virt -bios default --no-reboot -kernel kernel.elf
+# $QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot -kernel kernel.elf
